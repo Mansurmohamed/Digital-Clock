6432907224e1dfbbd323e6a6079a4b8159c7a698
Mansurmohamed <abdullamansur3@gmail.com>
2023-05-24 16:24:09 -0400

    quotes

--- DGClock.js
+++ DGClock.js
@@ -2,6 +2,30 @@ const hour = document.getElementById("hour");
 const minute = document.getElementById("minutes");
 const second = document.getElementById("Seconds");
 const TimeOfDay = document.getElementById("ampm");
+const Quotes = document.getElementById("QuotesBox");
+
+const getQuotes = () => {
+    let category = "happiness";
+    fetch("https://api.api-ninjas.com/v1/quotes?category=" + category, {
+        method: 'GET',
+        headers: {
+            'X-API-Key': 'qCz4qNTmsKcFofTADB3dfw==dAc5F727XnNUpkLY',
+            'Content-type': 'application/json'
+        }
+    })
+    .then(function(response){
+        if(!response.ok){
+            throw new Error('Network response not ok');
+        }
+        return response.json();
+    })
+    .then(function(result){
+       Quotes.innerText = result[0].quote;
+    })
+    .catch(function(error){
+        console.error('Error:', error);
+    })
+}
 
 const LiveClock = () => {
     let h = new Date().getHours()
@@ -22,9 +46,15 @@ const LiveClock = () => {
     minute.innerText = m;
     second.innerText = s;
     TimeOfDay.innerText = ampm;
-    setTimeout(()=> {
-        LiveClock()
-    }, 1000)
-}
+};
 
+getQuotes();
 LiveClock();
+
+setInterval(() => {
+     getQuotes();
+}, 16000);
+
+setInterval(() => {
+    LiveClock();
+}, 1000);
